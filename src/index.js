09c8592efe312706ffdb7ff71d023710f8fd9f1c
Raiymbet <tukpetov@bk.ru>
2021-08-05 22:35:44 +0600

    feat: solve Morse Decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+var letter_length = 10;
+const MORSE_DECODER_TABLE = {
+    10: '.',
+    11: '-',
+    '**********': ' '
+}
+
 function decode(expr) {
     // write your solution here
+    let result = '';
+
+    for (let i = 0; i < expr.length; i+=letter_length ) {
+        let part = expr.substring(i, letter_length + i),
+        morse_code = '';
+        if (part == '**********') {
+            result += MORSE_DECODER_TABLE[part];
+        } else {
+            for (let j = 0; j < part.length; j+=2) {
+                let code_ = part.substring(j, j+2);
+                if ( code_ != '00') {
+                    morse_code += MORSE_DECODER_TABLE[code_]
+                }
+            }
+            result += MORSE_TABLE[morse_code];
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
